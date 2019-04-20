59610bfc57302e4858fee200bb85fe7811ccce67
Gelena Barker <boldireg@tcd.ie>
2019-04-20 16:51:53 +0200

    individual place page styled

--- screens/PlaceScreen.js
+++ screens/PlaceScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Animated, View, StyleSheet, Image, Dimensions, ScrollView, Text, Button, TouchableOpacity, Platform, Linking } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 export default class PlacePage extends React.Component {
   constructor (props) {
@@ -31,38 +32,54 @@ export default class PlacePage extends React.Component {
   render () {
     return (
       <View >
-        <View style={styles.topView}>
-          <Text>{this.place.name}</Text>
-        </View>
 
         <ScrollView horizontal={true}>
 
-          {this.place.image.map(image => <View key={image.author} style={{ height: 27 }} style={styles.imagesBox}><Image
-            style={styles.images}
-            source={{ uri: image.url }}
-          /><Text>photo by {image.author}</Text></View>)}
+          {this.place.image.map((image, index) =>
+            <View key={index} style={{ height: 27 }} style={styles.imagesBox}>
+              <Image style={styles.images} source={{ uri: image.url }} />
+
+              <View style={styles.photoCredit}>
+                <Ionicons style={{ padding: 3 }} name='ios-camera' size={25} color='rgba(130,4,150, 0.6)' />
+                <Text>{image.author}</Text>
+              </View>
+
+            </View>)}
         </ScrollView>
 
-        <Text>{this.place.description}</Text>
-        <Button title='press' onPress={() => this._handlePressDirections()} />
+        <Text style={styles.description}>{this.place.description}</Text>
+
+        <View style={styles.directions}>
+          <Ionicons name='ios-pin' size={45} color='rgba(130,4,150, 0.9)' onPress={() => this._handlePressDirections()} ></Ionicons>
+          <Text>Get directions</Text>
+        </View>
       </View>
     )
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+<Ionicons style={{ padding: 10 }} name='ios-list' size={30} color='#000' onPress={() => { navigation.navigate('List') }}></Ionicons>
 
-  },
-  topView: {
-    flexDirection: 'column',
-  },
+const styles = StyleSheet.create({
+
   imagesBox: {
-    padding: 10,
+    padding: 6,
+    paddingTop: 25,
   },
   images: {
     width: 330,
     height: 380,
+  },
+  photoCredit: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  description: {
+    padding: 15,
+    alignSelf: 'center',
+  },
+  directions: {
+    alignSelf: 'center',
+    alignItems: 'center',
   }
 })
